e03263c46b8d7bbe036163e16dcf6c9c38b73a12
Rivaldi Putra <rivaldi.jakmall@gmail.com>
2022-03-08 12:28:42 +0800

    feat(login-page): create  global styles

--- apps/login-page/src/main.tsx
+++ apps/login-page/src/main.tsx
@@ -3,10 +3,12 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
+import { GlobalStyle } from './utils/Global';
 
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </StrictMode>,
