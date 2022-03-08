9dc809ed2f5b30e590b4fba759e7e56c3bb4c5c9
Rivaldi Putra <rivaldi.jakmall@gmail.com>
2022-03-09 06:07:52 +0800

    fix(login): implementation login user

--- apps/login-page/src/app/components/form-login/form-login.tsx
+++ apps/login-page/src/app/components/form-login/form-login.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Button, Link, Text, TextInput } from '@one-zero-group/ui-components';
+import useLoginForm from './useLoginForm';
 /* eslint-disable-next-line */
 export interface FormLoginProps {}
 
@@ -41,14 +42,33 @@ const StyledWrapperButton = styled.div`
 `;
 
 export function FormLogin(props: FormLoginProps) {
+  const { email, password, changeEmail, changePassword, login } =
+    useLoginForm();
   return (
     <StyledFormLogin>
-      <form action="">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          login();
+        }}
+      >
         <StyledFormControl>
-          <TextInput label="email" id="email" type="text" />
+          <TextInput
+            label="email"
+            id="email"
+            type="text"
+            value={email}
+            onChange={changeEmail}
+          />
         </StyledFormControl>
         <StyledFormControl>
-          <TextInput label="password" id="password" type="password" />
+          <TextInput
+            label="password"
+            id="password"
+            type="password"
+            value={password}
+            onChange={changePassword}
+          />
         </StyledFormControl>
         <StyledFormControl>
           <Link href="#">Forgot password?</Link>
@@ -58,7 +78,10 @@ export function FormLogin(props: FormLoginProps) {
             <Text color="#728197">New User?</Text>
             <Link href="#">Create account</Link>
           </div>
-          <Button variant="primary">Sign in</Button>
+          <Button type="submit" variant="primary">
+            Sign in
+          </Button>
+          {/* <button type="submit">Sign in</button> */}
         </StyledWrapperButton>
       </form>
     </StyledFormLogin>
